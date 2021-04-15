22617b17886e905945b54cc1671af050c228528f
1337968347 <1337968347@qq.com>
2021-04-15 21:56:45 +0800

    :star: vitural

--- src/components/virtual-scroll/util.ts
+++ src/components/virtual-scroll/util.ts
@@ -1,11 +1,13 @@
 interface VituralOption {
   rowDatas: any[];
   defaultHeight: number;
+  rootEl: HTMLElement;
 }
 
-export const createVirtualScanner = ({ rowDatas = [], defaultHeight = 30 }: VituralOption) => {
+export const createVirtualScanner = ({ rowDatas = [], defaultHeight = 30, rootEl }: VituralOption) => {
   const rowDataLen = rowDatas.length;
   const totalHeight = rowDataLen * defaultHeight;
+  const RootElement = rootEl;
 
   const getTotalHeight = () => {
     return totalHeight;
--- src/components/virtual-scroll/virtual-scroll.tsx
+++ src/components/virtual-scroll/virtual-scroll.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h, Host } from '@stencil/core';
+import { Component, Element, Prop, h, Host } from '@stencil/core';
 import { createVirtualScanner } from './util';
 
 @Component({
@@ -6,12 +6,16 @@ import { createVirtualScanner } from './util';
   styleUrl: 'virtual-scroll.scss',
 })
 export class VirtualScroll {
+  @Element() el: HTMLElement;
   @Prop() items: any[] = [];
   @Prop() itemRenderFn: (cell: any, itemIndex: Number) => HTMLElement;
   vituralOper: any = null;
 
+  componentWillLoad() {
+    this.vituralOper = createVirtualScanner({ rowDatas: this.items, defaultHeight: 40, rootEl: this.el });
+  }
+
   componentDidLoad() {
-    this.vituralOper = createVirtualScanner({ rowDatas: this.items, defaultHeight: 40 });
   }
 
   render() {
@@ -21,6 +25,12 @@ export class VirtualScroll {
           {this.items.map((item, index) => {
             return this.itemRenderFn(item, index);
           })}
+        </div>
+        <div class="vertical-scroll">
+          <div
+            style={{
+              height: this.vituralOper.getTotalHeight() + 'px',
+            }}></div>
         </div>
       </Host>
     );
