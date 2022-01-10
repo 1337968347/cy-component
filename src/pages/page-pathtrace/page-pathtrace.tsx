9ae3d8e073bd8f34dcb52d00c1b6aa84a930c234
陈阳 <1337968347@qq.com>
2022-01-10 21:54:50 +0800

    :star: tweaks

--- src/pages/page-pathtrace/page-pathtrace.tsx
+++ src/pages/page-pathtrace/page-pathtrace.tsx
@@ -11,7 +11,7 @@ export class PageVirtual {
         <cy-header>
           <cy-menu-button class="btn-box" slot="start" />
           <h3 class="cy-title">路径追踪（待补充）</h3>
-          <a target="_blank" href="https://github.com/1337968347/raytrace" slot="end">
+          <a target="_blank" href="https://github.com/1337968347/raytrace/blob/pathTrace/src/assets/shader/raytrace.frag" slot="end">
             <cy-icon name="github" />
           </a>
         </cy-header>
